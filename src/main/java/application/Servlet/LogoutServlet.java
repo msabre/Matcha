27c5c132b76a23422d87ede00457f76f4e308374
msabre <msabre@github.com>
2021-07-11 12:16:57 +0300

    Поправлена логика токен (no test)

--- src/main/java/application/Servlet/LogoutServlet.java
+++ src/main/java/application/Servlet/LogoutServlet.java
@@ -1,31 +1,14 @@
 package application.Servlet;
 
 import adapter.controller.JwtController;
-import adapter.controller.UserController;
 import application.services.HttpService;
-import application.services.json.JsonService;
 import config.MyConfiguration;
-import config.MyProperties;
-import domain.entity.User;
-import domain.entity.UserCard;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import sun.net.httpserver.HttpsServerImpl;
-
-import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import static config.MyConfiguration.userController;
 
 public class LogoutServlet extends HttpServlet {
 
---
+++ src/main/java/application/filters/AuthenticationFilter.java
@@ -0,0 +1,34 @@
+package application.filters;
+
+
+
+import domain.entity.User;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static java.util.Objects.nonNull;
+
+public class AuthenticationFilter implements Filter {
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+
+        User userWeb = (User) ((HttpServletRequest) req).getSession().getAttribute("user");
+        if (nonNull(userWeb) && userWeb.isAuthorized()) {
+            ((HttpServletResponse) resp).sendRedirect("/main");
+            return ;
+        }
+
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) {
+
+    }
+
+}
