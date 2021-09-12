549af2a1be692f35fbc2da7fe87c06618812198a
msabre <msabre@github.com>
2021-09-12 17:27:18 +0300

    Вынес добавление фильтра в Put метод

--- src/main/java/application/servlets/MainServlet.java
+++ src/main/java/application/servlets/MainServlet.java
@@ -37,23 +37,36 @@ public class MainServlet extends HttpServlet {
 
         switch (act) {
             case "getList":
-                String filters = HttpService.getBody(req);
                 User user = (User) req.getSession().getAttribute("user");
-                if (!filters.isEmpty()) {
-                    FilterParams filterParams = (FilterParams) JsonService.getObject(FilterParams.class, filters);
-                    filterParams.setId(user.getFilter().getId());
-                    if (isNull(filterParams.getLocation()))
-                        filterParams.setLocation(user.getLocation());
-
-                    userController.filterUpdate(filterParams);
-                    user.setFilter(filterParams);
-                    req.getSession().setAttribute("user", user);
-                }
                 List<User> usersList = userController.getRecommendUsersList(user);
                 HttpService.putBody(resp, JsonService.getJsonArray(usersList));
                 break;
             case "getContacts":
                 break;
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setCharacterEncoding("UTF-8");
+
+        String filters = HttpService.getBody(req);
+        User user = (User) req.getSession().getAttribute("user");
+
+        if (!filters.isEmpty()) {
+            FilterParams filterParams = (FilterParams) JsonService.getObject(FilterParams.class, filters);
+            if (filterParams.getRating() == null
+                    || filterParams.getCommonTagsCount() == null
+                    || filterParams.getAgeBy() == null
+                    || filterParams.getAgeTo() == null
+                    || filterParams.getLocation() == null) {
+                HttpService.putBody(resp, "NOT ALL REQUIRED FIELDS ARE FILLED IN");
+                return;
+            }
+            filterParams.setId(user.getFilter().getId());
+            userController.filterUpdate(filterParams);
+            user.setFilter(filterParams);
+            req.getSession().setAttribute("user", user);
         }
     }
 }
