987ded2ce3d2db2f43cb22166bf92a67fdb85b54
msabre <msabre@github.com>
2021-12-03 23:11:33 +0300

    Добавление фото при authUser

--- src/main/java/application/servlets/AddProfileInfoServlet.java
+++ src/main/java/application/servlets/AddProfileInfoServlet.java
@@ -17,10 +17,12 @@ import javax.imageio.stream.ImageOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 
 import java.util.*;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static config.MyConfiguration.userController;
@@ -45,7 +47,7 @@ public class AddProfileInfoServlet extends HttpServlet {
                 newCard.setPhotos(user.getCard().getPhotos());
                 newCard.setId(user.getCard().getId());
                 newCard.setUserId(user.getId());
-                
+
                 user.setCard(newCard);
                 userController.updateUserCard(newCard);
                 break;
@@ -65,8 +67,9 @@ public class AddProfileInfoServlet extends HttpServlet {
                     String photoParams = currentPhotoList.stream().map(ph -> String.format("%s_%s", ph.getNumber(), ph.getFormat())).collect(Collectors.joining(";"));
                     Integer mainPhoto = currentPhotoList.stream().filter(Photo::isMain).findFirst().map(ph -> Integer.parseInt(ph.getNumber())).orElse(null);
 
-                    if (mainPhoto != null || currentPhotoList.isEmpty())
-                        userController.updateMainPhoto(user.getCard().getId(), mainPhoto);
+                    if (mainPhoto != null || currentPhotoList.isEmpty()) {
+                        userController.updateMainPhoto(user.getCard().getId(), mainPhoto); 
+                    }
 
                     userController.updatePhotoParams(user.getCard().getId(), photoParams);
                 }
@@ -78,11 +81,14 @@ public class AddProfileInfoServlet extends HttpServlet {
     }
 
     private boolean processActionPhoto(List<Photo> photos, User user) {
+        boolean mainPhotoHasBennChange = false;
+        String oldMain = photos.stream().filter(Objects::nonNull).findFirst().map(Photo::getNumber).orElse(null);
+
         List<Photo> current = Optional.ofNullable(user.getCard().getPhotos()).orElse(new ArrayList<>(Collections.nCopies(5, null)));
         current.stream().filter(Objects::nonNull).filter(Photo::isMain).findFirst().ifPresent(photo -> photo.setMain(false));
 
         for (Photo photo : photos) {
-            String path = String.format("%sIMG_%s_photo_%s.jpg", MyProperties.IMAGES_PATH + MatchUtils.getSlash(), user.getId(), photo.getNumber());
+            String path = getPhotoPath(user, photo.getNumber(), false);
             int index = Optional.ofNullable(photo.getNumber()).map(Integer::parseInt).orElse(0) - 1;
             if (index < 0)
                 continue;
@@ -90,14 +96,17 @@ public class AddProfileInfoServlet extends HttpServlet {
             switch (photo.getAction()) {
                 case "save":
                     try {
+                        if (checkAndDeleteIfMain(user, photo, oldMain))
+                            mainPhotoHasBennChange = true;
                         byte[] byteContent = photo.getContent().getBytes();
                         if (JPG.equals(photo.getFormat())) {
                             ByteArrayInputStream bufferedInputStream = new ByteArrayInputStream(Base64.getDecoder().decode(byteContent));
                             BufferedImage img = ImageIO.read(bufferedInputStream);
+     
                             ImageIO.write(img, photo.getFormat(), new File(path));
                         }
                         else if (compressRequiredFormats.contains(photo.getFormat()))
-                            compressImage(byteContent, path);
+                            compressImage(byteContent, path, 600, 800);
                         else
                             return false;
 
@@ -109,27 +118,39 @@ public class AddProfileInfoServlet extends HttpServlet {
                     }
                     break;
                 case "delete":
+                    if (checkAndDeleteIfMain(user, photo, oldMain))
+                        mainPhotoHasBennChange = true;
                     File file = new File(path);
-                    if (file.exists() && file.delete())
+                    if (file.exists() && file.delete()) {
                         current.set(index, null);
+                    }
                 default:
                     break;
             }
         }
 
+        String newMainNum = "";
         Optional<Photo> mainPhoto = current.stream().filter(Objects::nonNull).filter(Photo::isMain).findFirst();
         if (!mainPhoto.isPresent()) {
-            current.stream().filter(Objects::nonNull).findFirst().ifPresent(ph -> ph.setMain(true));
+            mainPhoto = current.stream().filter(Objects::nonNull).findFirst();
+            
+            if (mainPhoto.isPresent()) {
+                newMainNum = mainPhoto.get().getNumber();
+                
+            }
         }
 
+        if (mainPhotoHasBennChange)
+            doNewMainPhoto(user, newMainNum);
+        
         return true;
     }
 
-
-    private static void compressImage(byte[] content, String destinationPath) throws IOException {
+    private static void compressImage(byte[] content, String destinationPath, int width, int height) throws IOException {
         ByteArrayInputStream bufferedInputStream = new ByteArrayInputStream(Base64.getDecoder().decode(content));
         BufferedImage img = ImageIO.read(bufferedInputStream);
 
+        Image resizeImage = img.getScaledInstance(width, height, Image.SCALE_DEFAULT);
         File compressedImageFile = new File(destinationPath);
         OutputStream os = new FileOutputStream(compressedImageFile);
 
@@ -142,12 +163,67 @@ public class AddProfileInfoServlet extends HttpServlet {
         ImageWriteParam param = writer.getDefaultWriteParam();
 
         param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-        param.setCompressionQuality(0.05f);
-        writer.write( null , new IIOImage(img, null , null ), param);
+        param.setCompressionQuality(0.08f);
+        writer.write( null , new IIOImage(convertToBufferedImage(resizeImage), null , null ), param);
 
         os.close();
         ios.close();
         writer.dispose();
+    }
+
+    public static BufferedImage convertToBufferedImage(Image img) {
+
+        if (img instanceof BufferedImage) {
+            return (BufferedImage) img;
+        }
+
+        // Create a buffered image with transparency
+        BufferedImage bi = new BufferedImage(
+                img.getWidth(null), img.getHeight(null),
+                BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D graphics2D = bi.createGraphics();
+        graphics2D.drawImage(img, 0, 0, null);
+        graphics2D.dispose();
+
+        return bi;
+    }
+    
+    private boolean checkAndDeleteIfMain(User user, Photo photo, String oldMain) {
+        if (photo.isMain() && photo.getNumber().equals(oldMain)) {
+            String mainPhotoPath = getPhotoPath(user, photo.getNumber(), true);
+            File oldMainFile = new File(mainPhotoPath);
+            if (oldMainFile.exists() && oldMainFile.delete()) {
+                System.out.println("Файл " + mainPhotoPath + " был удален");
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private void doNewMainPhoto(User user, String newMainNum) {
+        String photoPath =  getPhotoPath(user, newMainNum, false);
+        String newMainPath = getPhotoPath(user, newMainNum, true);
+
+        File file = new File(photoPath);
+        try {
+            byte[] fileInArray = new byte[(int) file.length()];
+            FileInputStream fis = new FileInputStream(photoPath);
+            fis.read(fileInArray);
+
+            ByteArrayInputStream bufferedInputStream = new ByteArrayInputStream(fileInArray);
+            BufferedImage img = ImageIO.read(bufferedInputStream);
+            Image resizeImage = img.getScaledInstance(400, 400, Image.SCALE_DEFAULT);
+
+            ImageIO.write(convertToBufferedImage(resizeImage), "jpg", new File(newMainPath));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private String getPhotoPath(User user, String photoNum, boolean main) {
+        return String.format("%sIMG_%s_%s_photo_%s.jpg", MyProperties.IMAGES_PATH + MatchUtils.getSlash(), main ? "MAIN" : "", user.getId(), photoNum);
     }
 
     @Override
